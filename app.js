bde42abe407c55c1530313593bd63e9a7aa77ca6
Khemraj Thapa <khyamay@gmail.com>
2014-01-15 16:50:30 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+$(function() {
+	var accordion = $("#accordion");
+	var headings = $("h2");
+	var paragraphs = $("p");
+	paragraphs.not(":first").hide();
+	accordion.on("click", "h2", function(){
+		var t = $(this);
+		var tPara = t.next();
+		if(!tPara.is(":visible")){
+			tPara.trigger("showParagraph");
+		}
+	accordion.on("showParagraph", "p", function(){
+		paragraphs.stop(true, true).slideUp("normal");
+		$(this).stop(true, true).slideDown("normal");
+	});
+		
+	});
+});
